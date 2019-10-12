682def6988d570a440ffa4578e7250e1f9eaa714
Simen A. W. Olsen <cobraz@cobraz.no>
2019-10-12 16:36:03 +0200

    fix: pg issue

--- models/index.js
+++ models/index.js
@@ -6,9 +6,15 @@ var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 if (env !== 'production') {
   var config    = require(__dirname + '/../config/config.json')[env];
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModule: require('sqlite3')
+  });
 } else {
-  var sequelize = new Sequelize(process.env.DATABASE_URL, {});
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "pg",
+    dialectModule: require("pg")
+  });
 }
 var db        = {};
 
