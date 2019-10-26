f8f6fbff4c947ce1817cbe60276e443f0c10e366
Simen A. W. Olsen <cobraz@cobraz.no>
2019-10-26 03:52:53 +0200

    feat: auth0

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ module.exports = function(sequelize, DataTypes) {
       accessToken: DataTypes.STRING,
       refreshToken: DataTypes.STRING,
       identification: DataTypes.INTEGER,
+      email: DataTypes.STRING,
     },
     {
       classMethods: {
